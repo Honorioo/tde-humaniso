cb0405c81e5e630172747a348828e8fd95a4aeab
Honorioo <viniciusgomeshonorio@hotmail.com>
2023-05-12 18:22:20 -0300

    segundo commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+function img1on(x) {
+    document.getElementById('img1').style.borderRadius = '1.3rem'; 
+    x.style.height = "60rem";
+    x.style.width = "75rem";
+     }
+     function img1out(x){
+        x.style.height = "60rem";
+        x.style.width = "15rem"
+    }
+
+    function img2on(x){
+        x.style.height = "60rem";
+        x.style.width = "75rem"
+    }
+    function img2out(x){
+        x.style.height = "60rem";
+        x.style.width = "15rem";
+    }
+
+
+
